938b16a1dc499407c5b7e20b8d6931381225465e
Shawn Xie <shawnxie920@gmail.com>
2022-11-07 15:14:31 -0500

    Improvements and file rename

--- db/migration/20221107_drop_issuer_nf_tokens.sh
+++ db/migration/20221107_drop_issuer_nf_tokens.sh
@@ -1,11 +1,13 @@
 #!/bin/bash 
-#Drops issuer_nf_tokens table and set latest ledger index to the one prior to XLS-20 amendment
+#Schema for issuer_nf_tokens table changed, with a new taxon column. Old table needs to be dropped
 
 XLS20_PRIOR_LEDGER_INDEX=75443456
 
-SEQUENCE_1=`cqlsh -e "SELECT sequence FROM clio.ledger_range WHERE is_latest=true;"`
+SEQUENCE_1=$(cqlsh -e "SELECT sequence FROM clio.ledger_range WHERE is_latest=true;")
+
 sleep 10s
-SEQUENCE_2=`cqlsh -e "SELECT sequence FROM clio.ledger_range WHERE is_latest=true;"`
+
+SEQUENCE_2=$(cqlsh -e "SELECT sequence FROM clio.ledger_range WHERE is_latest=true;")
 
 if [[ $SEQUENCE_1 !=  $SEQUENCE_2 ]]
 then
